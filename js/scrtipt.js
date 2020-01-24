e1168ee7de07afc2249ce9f43614e2c661ef1696
findus19 <ivanexs@mail.ru>
2020-01-24 21:04:59 +0300

    исправление

--- js/scrtipt.js
+++ js/scrtipt.js
@@ -14,8 +14,8 @@ console.log(addExpenses.length);
 console.log("Период равен",`${period}`, "месяцев" + ' и ' + 
     "Цель заработать", `${mission}`, "рублей/долларов/гривен/юани");
 
-addExpenses1 = addExpenses.toLowerCase();
-console.log(addExpenses1.split());
+/* addExpenses1 = addExpenses.toLowerCase(); */
+console.log(addExpenses.toLowerCase().split(','));
 
 let budgetDay = money / 30;
 console.log("Дневной бюджет = ", budgetDay);
